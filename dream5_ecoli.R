907198d5e29c4e483874b51f6868191c370162bc
Eric Kernfeld <eric.kern13@gmail.com>
2023-09-15 08:54:11 -0400

    Update main script wordking firectory

--- dream5_ecoli.R
+++ dream5_ecoli.R
@@ -1,15 +1,9 @@
-dir.create("~/Desktop/jhu/research/projects/knockoffs/applications/dream5_sa_ec/ecoli/v33/")
-setwd("~/Desktop/jhu/research/projects/knockoffs/applications/dream5_sa_ec/ecoli/v33/")
+# These lines are not needed when using the wrapper script run_on_aws, but they are useful for interacting with it in rstudio.
+# dir.create("~/Desktop/jhu/research/projects/knockoffs/applications/dream5_sa_ec/ecoli/v34/")
+# setwd("~/Desktop/jhu/research/projects/knockoffs/applications/dream5_sa_ec/ecoli/v34/")
 source("../dream5_ecoli_gold_standards.R")
 source("../dream5_ecoli_setup.R")
 dir.create("logs")
-
-# This is for internal use (interactive debugging).
-# do_one(1,  reuse_results = F, test_mode = T)
-do_one_safe(1,  reuse_results = F, test_mode = F)
-do_one_safe(3,  reuse_results = F, test_mode = F)
-do_one_safe(2,  reuse_results = F, test_mode = F)
-do_one_safe(4,  reuse_results = F, test_mode = F)
 
 # The full run.
 parallel::mclapply(
